e69513c9f4cafa7e5c783ccfc604ffecae81b654
talaslegraps <thihem@gmail.com>
2021-10-27 09:38:56 +0200

    implement route: '/' get all users

--- client.js
+++ client.js
@@ -1,12 +1,12 @@
-// const { Pool } = require("pg");
-// const pool = new Pool();
+const { Pool } = require("pg");
+const pool = new Pool();
 
-// module.exports = {
-//     query: (text, params) => {
-//         console.log({
-//             request: text,
-//             time: new Date().toLocaleTimeString(),
-//         });
-//         return pool.query(text, params);
-//     }
-// }
+module.exports = {
+  query: (text, params) => {
+    console.log({
+      request: text,
+      time: new Date().toLocaleTimeString(),
+    });
+    return pool.query(text, params);
+  },
+};
